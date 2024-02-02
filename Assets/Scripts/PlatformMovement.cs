a8968a71ee4763bb5e4381082712b28c321cfe54
Tessla Muir <tesslamuir@yahoo.com>
2024-02-01 22:58:56 -0500

    Moving platforms will move player

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -34,4 +34,22 @@ public class PlatformMovement : MonoBehaviour
         if (transform.position.x >= rightEnd) isMovingRight = false;
         if (transform.position.x <= leftEnd) isMovingRight = true;
     }
+
+    // Move the player if it's on top of platform
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            // Move player in platform direction
+            if (isMovingRight)
+            {
+                collision.gameObject.transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+            }
+            else
+            {
+                collision.gameObject.transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+            }
+            
+        }
+    }
 }
