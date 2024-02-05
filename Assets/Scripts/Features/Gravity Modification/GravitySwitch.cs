d95cbd85e58aab2f36d11a06293ba031af4a6e5b
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-02-04 20:05:30 -0500

    Update GravitySwitch.cs
    added comments and cleaned up the code

--- Assets/Scripts/Features/Gravity Modification/GravitySwitch.cs
+++ Assets/Scripts/Features/Gravity Modification/GravitySwitch.cs
@@ -5,53 +5,57 @@ namespace Features.Gravity_Modification
     [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
     public class GravitySwitch : MonoBehaviour
     {
+        [SerializeField] private GameObject gravityParticles;
+        
         private Rigidbody2D _rb;
-        SpriteRenderer _SpriteRenderer;
-        public GameObject gravityParticles;
-        ParticleSystem gravityPS;
+        private SpriteRenderer _spriteRenderer;
+        private ParticleSystem _gravityPS;
+        private Camera _mainCam;
 
-        // Start is called before the first frame update
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
         private void Start()
         {
             // Initialize components
             _rb = GetComponent<Rigidbody2D>();
-            _SpriteRenderer = GetComponent<SpriteRenderer>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _mainCam = Camera.main;
 
             // Get max height and set particles to bottom
-            float maxHeight = Camera.main.GetComponent<Camera>().orthographicSize;
-            gravityParticles.transform.position = new Vector3(Camera.main.transform.position.x, -maxHeight, 0.0f);
+            gravityParticles.transform.position = new Vector3(_mainCam.transform.position.x, -_mainCam.orthographicSize, 0.0f);
 
             // Get particles from game object
-            gravityPS = gravityParticles.GetComponentInChildren<ParticleSystem>();
-
+            _gravityPS = gravityParticles.GetComponentInChildren<ParticleSystem>();
         }
 
+        /// <summary>
+        /// Update is called once every frame.
+        /// </summary>
         private void Update()
         {
-            if(Input.anyKey && Input.GetKeyDown(KeyCode.G) && _rb.velocity.y == 0.0f)
+            if (Input.anyKey && Input.GetKeyDown(KeyCode.G) && _rb.velocity.y == 0.0f)
                 FlipObjectGravity();
         }
 
+        /// <summary>
+        /// Function called to flip the player's gravity.
+        /// </summary>
         private void FlipObjectGravity()
         {
-            _rb.gravityScale = -_rb.gravityScale;
+            // Reverse the gravity
+            var gravityScale = _rb.gravityScale;
+            gravityScale = -gravityScale;
+            _rb.gravityScale = gravityScale;
 
             // Get max height and properly set particle position/rotation
-            float maxHeight = Camera.main.GetComponent<Camera>().orthographicSize;
-            if(_rb.gravityScale <= 0.0f)
-            {
-                gravityParticles.transform.position = Camera.main.transform.position + new Vector3(0.0f, -maxHeight, 1.0f);
-                _SpriteRenderer.flipY = true;
-            }
-            else
-            {
-                gravityParticles.transform.position = Camera.main.transform.position + new Vector3(0.0f, maxHeight, 1.0f);
-                _SpriteRenderer.flipY = false;
-            }
-            gravityParticles.transform.rotation *= Quaternion.AngleAxis(180.0f, new Vector3(0.0f, 0.0f, 1.0f));
+            gravityParticles.transform.position = _mainCam.transform.position
+                                                  + new Vector3(0f, Mathf.Sign(gravityScale) * _mainCam.orthographicSize, 1f);
+            _spriteRenderer.flipY = gravityScale <= 0f;
+            gravityParticles.transform.rotation *= Quaternion.AngleAxis(180f, Vector3.forward);
 
             // Play particles
-            gravityPS.Play();
+            _gravityPS.Play();
         }
     }
 }
