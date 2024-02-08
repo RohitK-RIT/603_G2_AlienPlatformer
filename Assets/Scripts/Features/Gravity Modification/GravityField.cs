0ad8b6776007d0d7bf6c1be71a020c9a7e17eb4f
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-02-08 04:23:38 -0500

    Gravity Field implemented
    changing world gravity randomly between 0.5 and 1.5 times normal gravity

---
+++ Assets/Scripts/Features/Gravity Modification/GravityField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Features.Gravity_Modification
+{
+    [RequireComponent(typeof(EdgeCollider2D))]
+    public class GravityField : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<EdgeCollider2D>().isTrigger = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            Physics2D.gravity = Vector2.down * 9.8f * Random.Range(0.5f, 1.5f);
+        }
+    }
+}
