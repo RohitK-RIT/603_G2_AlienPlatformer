e70558d39579b8bfb750aec242edf2e323639e21
Heena Thadani <heenathadani14@gmail.com>
2024-02-15 14:47:23 -0500

    updated text in ui-onboarding prefab

--- Assets/UI_Scripts/OnboardingInstructions.cs
+++ Assets/UI_Scripts/OnboardingInstructions.cs
@@ -59,25 +59,17 @@ public class OnboardingInstructions : MonoBehaviour
                 var yAxis = Input.GetAxis("Vertical");
 
                 if (xAxis < 0 && instructions[currentInstructionIndex]
-                        .Contains(
-                            "The aliens have infiltrated the human space station orbiting the planet, determined to fight back and liberate their home. Use the \"A\" Key to Move Left"))
-                {
-                    keyPressed = true;
-                }
-                else if (xAxis > 0 &&
-                         instructions[currentInstructionIndex].Contains("and \"D\" Key to maneuver right through the station's intricate corridors."))
+                        .Contains("Use the \"A & D\" Key for Movement."))
                 {
                     keyPressed = true;
                 }
                 else if (yAxis > 0 && instructions[currentInstructionIndex]
-                             .Contains(
-                                 "Press \"W'' to leap over obstacles that lie in their path. Your mission is to guide a resourceful robot ally, a key figure in their resistance, navigating through a maze of challenges to reach the finish line. "))
+                             .Contains("Press \"W'' to Jump."))
                 {
                     keyPressed = true;
                 }
                 else if (Input.GetKeyDown(KeyCode.Space) && instructions[currentInstructionIndex]
-                             .Contains(
-                                 "Each alien can manipulate gravity at will, a crucial skill for their mission of liberation. By pressing \"Space Bar\" players can change gravity's direction, allowing aliens to traverse ceilings and vertical surfaces, bypassing obstacles and evading security measures."))
+                             .Contains("Use \"Space Bar\" to Switch Gravity."))
                 {
                     keyPressed = true;
                 }
